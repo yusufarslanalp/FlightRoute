1cb159a9ce73494f5c42e3cb424355033274056b
yusufarslanalp <ayusufa42@gmail.com>
2026-03-18 01:33:37 +0300

    feat: add liquibase.

---
+++ src/main/java/com/example/FlightRoute/config/LiquibaseConfig.java
@@ -0,0 +1,20 @@
+package com.example.FlightRoute.config;
+
+import javax.sql.DataSource;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+        return liquibase;
+    }
+}
+
